b4df8c65c352e143b60eaa318a5aba3dbcfca741
AhmedZein <ahmedzain503@gmail.com>
2022-09-19 18:50:50 +0200

    adding mangers schema

--- migrations/sqls/20220919145745-mangers-up.sql
+++ migrations/sqls/20220919145745-mangers-up.sql
@@ -1,1 +1,9 @@
 /* Replace with your SQL commands */
+CREATE TABLE mangers (
+
+    NAME VARCHAR (100) NOT NULL,
+    START_DATE date DEFAULT now(),
+    END_DATE date DEFAULT NULL, 
+    MANGER_ID INT PRIMARY KEY
+
+);
