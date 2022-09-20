898276f9d4078e22051409b769e19749443c03ca
AhmedZein <ahmedzain503@gmail.com>
2022-09-20 14:54:25 +0200

    adding teachers migrations

--- migrations/sqls/20220919145807-teachers-up.sql
+++ migrations/sqls/20220919145807-teachers-up.sql
@@ -1,1 +1,8 @@
 /* Replace with your SQL commands */
+CREATE TABLE teachers (
+    NAME VARCHAR(100) NOT NULL,
+    START_DATE date DEFAULT now(),
+    END_DATE date DEFAULT NULL
+    SALARY INT NOT NULL,
+    teacher_id INT PRIMARY KEY
+);
