bba1f8847c7b5c0c284e70f8861b4d0f3c6c5467
AhmedZein <ahmedzain503@gmail.com>
2022-09-20 21:39:56 +0200

    fixing manager table name in the migration

--- migrations/sqls/20220919145745-mangers-up.sql
+++ migrations/sqls/20220919145745-mangers-up.sql
@@ -1,8 +1,8 @@
 /* Replace with your SQL commands */
-CREATE TABLE mangers (
+CREATE TABLE managers (
     NAME VARCHAR (100) NOT NULL,
     START_DATE date DEFAULT now(),
     END_DATE date DEFAULT NULL,
     SALARY INT NOT NULL, 
-    MANGER_ID INT PRIMARY KEY
+    MANAGER_ID INT PRIMARY KEY
 );
