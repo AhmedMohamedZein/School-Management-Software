dd75888e358127e9b953a3418d27f548c2ec1943
AhmedZein <ahmedzain503@gmail.com>
2022-09-20 20:00:05 +0200

    adding students migrations schema

--- migrations/sqls/20220919145825-students-up.sql
+++ migrations/sqls/20220919145825-students-up.sql
@@ -1,1 +1,11 @@
 /* Replace with your SQL commands */
+/* end date should be filed when the student graduated or exits the school */
+CREATE TABLE students (
+    NAME VARCHAR(100) NOT NULL,
+    START_DATE date DEFAULT now(),
+    END_DATE date DEFAULT NULL,
+    PHONE_NUMBER VARCHAR(15) DEFAULT NULL,
+    EMAIL VARCHAR(30) NOT NULL,
+    STUDENT_ID INT PRIMARY KEY
+    /* STUDENT_ID should be the national id*/
+);
