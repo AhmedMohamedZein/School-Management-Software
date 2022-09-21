f303303d027bcdb6262ab3399ef7613f16c08beb
AhmedZein <ahmedzain503@gmail.com>
2022-09-21 19:42:53 +0200

    adding the show end point in manager handler

--- src/handlers/mangers.ts
+++ src/handlers/mangers.ts
@@ -3,10 +3,11 @@ import { manager , managersConnectionDB } from '../modules/mangers';
 
 
 const mangerHandler = async ( app : Application ) : Promise<void> => {
-    app.get ('/managers', index);
     // this endpint will return all mangers 
     // it will be helpful in the front page of the websit
-
+    app.get ('/managers', index);
+    // the managers by id will be used by the internal system to get the manager after authrization and authentication 
+    app.get('/managers/:id', show);
 
 }
 
@@ -25,5 +26,21 @@ async function index( req : Request , res : Response ) : Promise<void>{
 
 }
 
+async function show( req : Request , res : Response ) : Promise<void>{
+    const requestedManager : manager = { 
+        manager_id : req.params.id
+    };
+    try {
+        const managers =  new managersConnectionDB();
+        const requestedData = await managers.show(requestedManager);
+        res.status(200).json( requestedData ).end();
+        return;
+    }
+    catch (error) {
+        res.status(500).json('server error 500').end();
+        return;
+    }
+
+}
 
 export default mangerHandler ;
--- src/modules/mangers.ts
+++ src/modules/mangers.ts
@@ -21,8 +21,18 @@ class managersConnectionDB {
         }
     }
 
-
-
+    async show (requestedManager : manager) : Promise<manager | Error> {
+        try {
+            const conn = await client.connect() ; 
+            const sql = 'SELECT * FROM managers WHERE MANAGER_ID = $1';
+            const returnedData = await conn.query (sql , [ requestedManager.manager_id ] ) ;
+            return returnedData.rows[0] ;
+        }
+        catch (error) {
+            console.log (`error happend in managersConnectionDB-> index ${error}`);
+            return new Error (`Error message : ${error}`);
+        }
+    }
 
 }
 
